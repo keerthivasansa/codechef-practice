a339a3b02aaa5db0aa7bd7f179a9340fb0a0411b
Keerthi Vasan <sakeerthi23@gmail.com>
2024-03-30 18:56:23 +0530

    fix: broken import in routes

--- app/api/problems/route.ts
+++
@@ -1,7 +0,0 @@
-import { createProblem } from "@/app/lib/actions";
-
-export async function GET(req: Request) {
-    const prob = await createProblem();
-    console.log({ prob })
-    return Response.json(prob);   
-}
